d7a467a751e4d7fe573ce70677957a0783f4392e
Julian Trommer <julian.trommer@web.de>
2021-04-06 10:10:03 +0200

    Added more scenarios

--- scoomatic_scenario/src/scoomatic_scenario/scoomatic_scenario.py
+++ scoomatic_scenario/src/scoomatic_scenario/scoomatic_scenario.py
@@ -5,6 +5,8 @@
 # This work is licensed under the terms of the MIT license.
 # For a copy, see <https://opensource.org/licenses/MIT>.
 
+import time
+
 import os
 import rospy
 import carla
@@ -26,7 +28,7 @@ class ScoomaticScenario(object):
 	    rospy.get_param("~host", "localhost"), rospy.get_param("~port", "2000"))
 	self.client.set_timeout(10.0)
 	self.walker = None
-	self.walker_controller = None
+	self.previous_scenario = 0
 
         # ==========================================
         # -- Subscriber ----------------------------
@@ -49,57 +51,102 @@ class ScoomaticScenario(object):
 
 
     def execute_scenario(self, scenario):
-	if scenario.data == 1:
+	if scenario.data == 0:
+	    self.execute_previous_scenario()
+	elif scenario.data == 1:
+	    self.previous_scenario = 1
 	    self.execute_scenario_one()
-	if scenario.data == 2:
-	    self.stop_scenario()
-	    self.execute_scenario_one()
+	elif scenario.data == 2:
+	    self.previous_scenario = 2
+	    self.execute_scenario_two()
+	elif scenario.data == 3:
+	    self.previous_scenario = 3
+	    self.execute_scenario_three()
+
+    def execute_previous_scenario(self):
+	if self.previous_scenario > 0:
+	    if self.previous_scenario == 1:
+		self.execute_scenario_one()
+	    elif self.previous_scenario == 2:
+		self.execute_scenario_two()
+	    elif self.previous_scenario == 3:
+		self.execute_scenario_three()
+	else:
+	    rospy.logwarn("There was no scenario executed previously!")
 
     def execute_scenario_one(self):
 	rospy.loginfo("Executing scenario 1...")
+	self.set_scoomatic_pose(-18.85, 49.13, -1.4, 0.00, 0.00, 0.71, 0.70)
+	self.spawn_walker(-19.25, -79.60, 1.3, 0.0, 90.0, 0.0, -0.025, 1.0, 0.0, 0.25)
+	self.start_scenario(-19.25, 79.60, 1.3, 0.00, 0.00, 0.71, 0.70)
+
+    def execute_scenario_two(self):
+	rospy.loginfo("Executing scenario 2...")
+	self.set_scoomatic_pose(-16.78, 66.92, -1.4, 0.00, 0.00, 1.0, 0.02)
+	self.spawn_walker(-28.96, -75.45, 1.3, 0.0, 90.0, 0.0, -0.025, 1.0, 0.0, 0.25)
+	self.start_scenario(-41.13, 68.14, -1.4, 0.00, 0.00, 1.00, 0.02)
+
+    def execute_scenario_thre(self):
+	rospy.loginfo("Executing scenario 3...")
+	self.set_scoomatic_pose(-16.78, 66.92, -1.4, 0.00, 0.00, 1.0, 0.02)
+	self.spawn_walker(-28.96, -55.45, 1.3, 0.0, 270.0, 0.0, 0.025, -1.0, 0.0, 0.25)
+	self.start_scenario(-41.13, 68.14, -1.4, 0.00, 0.00, 1.00, 0.02)
+
+
+    def set_scoomatic_pose(self, pos_x, pos_y, pos_z, orient_x, orient_y, orient_z, orient_w):
 	# set the pose of the scoomatic
-	self.pose = PoseWithCovarianceStamped()
-	self.pose.pose.pose.position.x = -18.85
-	self.pose.pose.pose.position.y = 49.13
-	self.pose.pose.pose.position.z = -1.4
-	self.pose.pose.pose.orientation.x = 0.00
-	self.pose.pose.pose.orientation.y = 0.00
-	self.pose.pose.pose.orientation.z = 0.71
-	self.pose.pose.pose.orientation.w = 0.70
-	self.initialpose_pub.publish(self.pose)
+	pose = PoseWithCovarianceStamped()
+	pose.header.stamp = rospy.Time.now()
+	pose.pose.pose.position.x = pos_x
+	pose.pose.pose.position.y = pos_y
+	pose.pose.pose.position.z = pos_z
+	pose.pose.pose.orientation.x = orient_x
+	pose.pose.pose.orientation.y = orient_y
+	pose.pose.pose.orientation.z = orient_z
+	pose.pose.pose.orientation.w = orient_w
+	self.initialpose_pub.publish(pose)
+
+    def spawn_walker(self, loc_x, loc_y, loc_z, pitch, yaw, roll, walk_x, walk_y, walk_z, walk_speed):
 	# spawn the walker
 	world = self.client.get_world()
 	walker_bp = world.get_blueprint_library().filter("walker.pedestrian.*")[0]
 	walker_bp.set_attribute('is_invincible', 'false')
 	walker_sp = carla.Transform()
-	walker_sp.location.x = -19.25
-	walker_sp.location.y = -79.60
-	walker_sp.location.z = 1.3
-	walker_sp.rotation.pitch = 0.0
-	walker_sp.rotation.yaw = 90.0
-	walker_sp.rotation.roll = 0.0
-	
+	walker_sp.location.x = loc_x
+	walker_sp.location.y = loc_y
+	walker_sp.location.z = loc_z
+	walker_sp.rotation.pitch = pitch
+	walker_sp.rotation.yaw = yaw
+	walker_sp.rotation.roll = roll
+
 	self.walker = world.spawn_actor(walker_bp, walker_sp)
 
+	time.sleep(5)
+
 	#spawn the walker control
-	self.walker_control = carla.WalkerControl(carla.Vector3D(-0.025, 1.0, 0.0), 0.25, False)
-	self.walker.apply_control(self.walker_control)
+	walker_control = carla.WalkerControl(carla.Vector3D(walk_x, walk_y, walk_z), walk_speed, False)
+	self.walker.apply_control(walker_control)
 	world.wait_for_tick()
 
+    def start_scenario(self, pos_x, pos_y, pos_z, orient_x, orient_y, orient_z, orient_w):
 	# start the scenario
-	self.goal = PoseStamped()
-	self.goal.header.frame_id = "map"	
-	self.goal.pose.position.x = -19.25
-	self.goal.pose.position.y = 79.60
-	self.goal.pose.position.z = 1.3
-	self.goal.pose.orientation.x = 0.00
-	self.goal.pose.orientation.y = 0.00
-	self.goal.pose.orientation.z = 0.71
-	self.goal.pose.orientation.w = 0.70
-	self.goal_pub.publish(self.goal)
+	goal = PoseStamped()
+	goal.header.stamp = rospy.Time.now()
+	goal.header.frame_id = "map"	
+	goal.pose.position.x = pos_x
+	goal.pose.position.y = pos_y
+	goal.pose.position.z = pos_z
+	goal.pose.orientation.x = orient_x
+	goal.pose.orientation.y = orient_y
+	goal.pose.orientation.z = orient_z
+	goal.pose.orientation.w = orient_w
+	self.goal_pub.publish(goal)
+
 
     def stop_scenario(self):
-	self.walker.destroy()
+	self.walker.delete()
+	del walker
+
 
     def __del__(self):
         """
